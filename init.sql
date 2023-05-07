0c4649f1eccd0cf7bce86efebede18d4d4669620
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2023-05-06 23:08:01 -0300

    Se puede insertar y leer de la base a través de la app, se puede enviar mensajes por rabbit y que se inserten automaticamente, se pueden generar mensajes en un batch cada x segundos, DatabaseManager para manejar los accesos a la base, modifico init.sql con las tablas iniciales, agrego rabbit al docker-compose.yml

--- init.sql
+++ init.sql
@@ -1,6 +1,33 @@
-CREATE TABLE measurement_data (
-    id int PRIMARY KEY,
-    producer_id int,
-    date datetime,
-    value int
+CREATE TABLE medida
+(
+    id           int PRIMARY KEY auto_increment,
+    sensor_id    int      NOT NULL,
+    fecha        datetime NOT NULL,
+    valor_medido int      NOT NULL,
+    FOREIGN KEY (sensor_id) REFERENCES sensor (id)
+);
+
+CREATE TABLE sensor
+(
+    id        int PRIMARY KEY auto_increment,
+    ubicacion VARCHAR(40)
+);
+
+CREATE TABLE alerta
+(
+    id             int PRIMARY KEY auto_increment,
+    sensor_id      int      NOT NULL,
+    fecha          datetime NOT NULL,
+    valor_medido   int      NOT NULL,
+    valor_esperado int      NOT NULL,
+    FOREIGN KEY (sensor_id) REFERENCES sensor (id),
+    FOREIGN KEY (fecha) REFERENCES medida (fecha)
+);
+
+CREATE TABLE usuario
+(
+    email    VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(20) NOT NULL,
+    status   VARCHAR(20) NOT NULL,
+    salt     VARCHAR(4)  NOT NULL
 );
