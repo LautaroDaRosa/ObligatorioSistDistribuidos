d5f4b867eb3cb1b545faf1d77d0035c03765803b
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2023-05-09 21:46:32 -0300

    fix db docker-compose & initb.d.sql

--- mysql/initb.d.sql
+++ mysql/initb.d.sql
@@ -1,33 +1,35 @@
-CREATE TABLE medition
+USE `test-db`;
+CREATE TABLE IF NOT EXISTS sensor
 (
-    id           int PRIMARY KEY auto_increment,
-    sensor_id    int      NOT NULL,
-    date        datetime NOT NULL,
-    value int      NOT NULL,
+    id        int auto_increment,
+    ubication VARCHAR(40),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS medition
+(
+    sensor_id int      NOT NULL,
+    date      datetime NOT NULL,
+    value     int      NOT NULL,
+    PRIMARY KEY (sensor_id, date),
     FOREIGN KEY (sensor_id) REFERENCES sensor (id)
 );
 
-CREATE TABLE sensor
+CREATE TABLE IF NOT EXISTS alerta
 (
-    id        int PRIMARY KEY auto_increment,
-    ubication VARCHAR(40)
-);
-
-CREATE TABLE alerta
-(
-    id             int PRIMARY KEY auto_increment,
     sensor_id      int      NOT NULL,
-    date          datetime NOT NULL,
-    analyzed_value   int      NOT NULL,
+    date           datetime NOT NULL,
+    analyzed_value int      NOT NULL,
     expected_value int      NOT NULL,
-    FOREIGN KEY (sensor_id) REFERENCES sensor (id),
-    FOREIGN KEY (date) REFERENCES medida (date)
+    PRIMARY KEY (sensor_id, date),
+    FOREIGN KEY (sensor_id, date) REFERENCES medition (sensor_id, date)
 );
 
-CREATE TABLE user
+CREATE TABLE IF NOT EXISTS user
 (
-    email    VARCHAR(30) PRIMARY KEY,
+    email    VARCHAR(30) NOT NULL,
     password VARCHAR(20) NOT NULL,
     status   VARCHAR(20) NOT NULL,
-    salt     VARCHAR(4)  NOT NULL
+    salt     VARCHAR(4)  NOT NULL,
+    PRIMARY KEY (email)
 );
