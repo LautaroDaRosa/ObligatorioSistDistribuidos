2776736b51909b50b558a04636c14beb1aaea168
LautaroDaRosa <lautaro.darosa@correo.ucu.edu.uy>
2023-06-19 22:36:10 -0300

    Agrego user por default en la base para el sensor, y agrego para que el sensor se autentique para poder insertar

--- services/sensor/src/main.py
+++ services/sensor/src/main.py
@@ -4,17 +4,25 @@ import logging
 import random
 import sys
 import time
-import pytz
 
+import pytz
 import requests
 import schedule
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-def send_request(message):
-    url = 'http://api-gateway:8080/insert_data'
+def authenticate():
+    url = 'http://api-gateway:8080/get_token'
+    message = {"username": "sensor01",
+               "password": "99l0*2%aeX4Z"}
     return requests.post(url=url, json=message)
+
+
+def send_request(message, authToken):
+    url = 'http://api-gateway:8080/insert_data'
+    bearerToken = "Bearer %s" % authToken
+    return requests.post(url=url, json=message, headers={'Authorization': bearerToken})
 
 
 def batch_job():
@@ -22,7 +30,9 @@ def batch_job():
     uruguay_timezone = pytz.timezone('America/Montevideo')
     randomValue = random.randint(10, 50)
     message = json.dumps((1, datetime.datetime.now(tz=uruguay_timezone).strftime("%Y-%m-%d %H:%M:%S"), randomValue))
-    send_request(message)
+    jwtResponse = authenticate()
+    authToken = jwtResponse.content.decode().split(':')[1].replace('}', '').replace('"', '')
+    send_request(message, authToken)
     logging.info("[x] Mensaje enviado: %s " % message)
 
 
